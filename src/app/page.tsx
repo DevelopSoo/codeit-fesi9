17df650784a18c0ff022fa7a4a1d61e4c004582c
DevelopSoo <69490984+DevelopSoo@users.noreply.github.com>
2025-05-13 13:48:47 +0900

    환경변수 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,10 @@ import SnapshotButton from "@/components/SnapshotButton";
 
 export default function Home() {
   return (
-    <h1>
-      게시글 목록
+    <>
+      <h1>게시글 목록</h1>
       <SnapshotButton>클릭</SnapshotButton>
-    </h1>
+      <div>내 환경: {process.env.NEXT_PUBLIC_MY_ENVIRONMENT}</div>
+    </>
   );
 }
