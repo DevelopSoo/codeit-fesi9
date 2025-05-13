713c21e6ccd8f0fd88a6186802aa9b37ad2b7348
DevelopSoo <69490984+DevelopSoo@users.noreply.github.com>
2025-05-13 13:08:40 +0900

    test

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -3,5 +3,5 @@ import Home from "./page";
 
 test("홈페이지 접속 시 상품 목록 페이지로 이동한다.", () => {
   render(<Home />);
-  expect(screen.getByText("게시글 목록")).toBeInTheDocument();
+  expect(screen.getByText("게시글 목록2")).toBeInTheDocument();
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import SnapshotButton from "@/components/SnapshotButton";
 export default function Home() {
   return (
     <>
-      <h1>게시글 목록</h1>
+      <h1>게시글 목록2</h1>
       <SnapshotButton>클릭</SnapshotButton>
     </>
   );
