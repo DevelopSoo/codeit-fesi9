866e1b191f8fb1322ffa5c3caab6a66144f67d93
DevelopSoo <69490984+DevelopSoo@users.noreply.github.com>
2025-05-13 10:27:40 +0900

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import SnapshotButton from "@/components/SnapshotButton";
 export default function Home() {
   return (
     <h1>
-      게시글 목록
+      게시글 목록 수정
       <SnapshotButton>클릭</SnapshotButton>
     </h1>
   );
