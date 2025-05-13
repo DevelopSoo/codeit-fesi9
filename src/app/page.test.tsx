3e34869f2d97347ecb2336680825129138a9771f
DevelopSoo <69490984+DevelopSoo@users.noreply.github.com>
2025-05-13 13:06:08 +0900

    test

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -1,3 +1,7 @@
+import { render, screen } from "@testing-library/react";
+import Home from "./page";
+
 test("홈페이지 접속 시 상품 목록 페이지로 이동한다.", () => {
-  expect(false).toBe(false);
+  render(<Home />);
+  expect(screen.getByText("게시글 목록")).toBeInTheDocument();
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import SnapshotButton from "@/components/SnapshotButton";
 
 export default function Home() {
   return (
-    <h1>
-      게시글 목록 수정
+    <>
+      <h1>게시글 목록</h1>
       <SnapshotButton>클릭</SnapshotButton>
-    </h1>
+    </>
   );
 }
